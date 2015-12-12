4fa664fef2d0be4c86b37ac3035cdfdf85a9bead
Marc Chapman <marc@bloodknightstudios.co.uk>
2015-12-12 01:59:01 +0000

    formatting OCD :/

--- scripts/server/game.cs
+++ scripts/server/game.cs
@@ -61,42 +61,42 @@ function onServerCreated()
 
    // Run the other gameplay scripts in this folder
    // Load up all scripts.  This function is called when
-// a server is constructed.
-exec("./camera.cs");
-exec("./triggers.cs");
-exec("./inventory.cs");
-exec("./shapeBase.cs");
-exec("./item.cs");
-exec("./health.cs");
-exec("./projectile.cs");
-exec("./radiusDamage.cs");
-exec("./teleporter.cs");
+	// a server is constructed.
+	exec("./camera.cs");
+	exec("./triggers.cs");
+	exec("./inventory.cs");
+	exec("./shapeBase.cs");
+	exec("./item.cs");
+	exec("./health.cs");
+	exec("./projectile.cs");
+	exec("./radiusDamage.cs");
+	exec("./teleporter.cs");
 
-// Load our supporting weapon script, it contains methods used by all weapons.
-exec("./weapon.cs");
+	// Load our supporting weapon script, it contains methods used by all weapons.
+	exec("./weapon.cs");
 
-// Load our weapon scripts
-// We only need weapon scripts for those weapons that work differently from the
-// class methods defined in weapon.cs
-exec("./proximityMine.cs");
+	// Load our weapon scripts
+	// We only need weapon scripts for those weapons that work differently from the
+	// class methods defined in weapon.cs
+	exec("./proximityMine.cs");
 
-// Load our default player script
-exec("./player.cs");
+	// Load our default player script
+	exec("./player.cs");
 
-// Load our player scripts
-exec("./aiPlayer.cs");
+	// Load our player scripts
+	exec("./aiPlayer.cs");
 
-exec("./vehicle.cs");
-exec("./vehicleWheeled.cs");
-exec("./cheetah.cs");
+	exec("./vehicle.cs");
+	exec("./vehicleWheeled.cs");
+	exec("./cheetah.cs");
 
-// Load turret support scripts
-exec("./turret.cs");
+	// Load turret support scripts
+	exec("./turret.cs");
 
-// Load our gametypes
-exec("./gameTypes/gameCore.cs"); // This is the 'core' of the gametype functionality.
-exec("./gameTypes/gameDM.cs"); // Overrides GameCore with DeathMatch functionality.
-exec("./gameTypes/gamePaintballDM.cs"); //comment this out to deactivate paintball
+	// Load our gametypes
+	exec("./gameTypes/gameCore.cs"); // This is the 'core' of the gametype functionality.
+	exec("./gameTypes/gameDM.cs"); // Overrides GameCore with DeathMatch functionality.
+	exec("./gameTypes/gamePaintballDM.cs"); //comment this out to deactivate paintball
 
    // Keep track of when the game started
    $Game::StartTime = $Sim::Time;
